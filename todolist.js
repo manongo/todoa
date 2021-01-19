7316514580f8bc8bbea4b50f75d231a82bd1aaf7
manongo <74598669+manongo@users.noreply.github.com>
2021-01-19 15:41:25 +0100

    Update todolist.js

--- todolist.js
+++ todolist.js
@@ -7,7 +7,7 @@ function ajoutTodo() {
     let data = {
         description: todo.value
     }
-    fetch('http://manongo.pythonanywhere.com/todo', {
+    fetch('https://manongo.pythonanywhere.com/todo', {
         method: 'POST',
         headers: {
             "Content-type": 'application/json'
@@ -17,14 +17,14 @@ function ajoutTodo() {
 }
 
 function deleteTodo(indice) {
-    fetch('http://manongo.pythonanywhere.com/todo/' + indice, {
+    fetch('https://manongo.pythonanywhere.com/todo/' + indice, {
         method: 'DELETE'})
         .then(response => rafraichirHTML())
 }
 
 function rafraichirHTML() {
     todo.value = "";
-    fetch('http://manongo.pythonanywhere.com/todo')
+    fetch('https://manongo.pythonanywhere.com/todo')
         .then(response => response.json())
         .then(todolist => {
             elementTODOLIST.innerHTML= "";
